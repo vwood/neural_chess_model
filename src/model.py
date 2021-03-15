9765c225ad95dac78ef421875fd3ff019100a104
V Wood <vwood.org@gmail.com>
2021-03-16 00:00:05 +1300

    Turn representations into classes

--- src/model.py
+++ src/model.py
@@ -19,81 +19,104 @@ TODO:
 
 piece_to_index = dict([(piece, i) for i, piece in enumerate('rnbqkpPRNBQK')])
 
-def simple_representation(fen):
+class BasicRepresentation:
     """
     * requires (12, 8, 8) input size
 
     12 panes, one per type of piece
     """
-    result = np.zeros((12, 8, 8), dtype=np.int8)
-
-    is_flipped = False
-    if fen.split(' ')[1] == 'b':
-        fen = fen.swapcase()
-        is_flipped = True
+    def get_input_size(self):
+        return (12, 8, 8)
     
-    row, col = 0, 0
-    for ch in fen:
-        if ch == ' ':
-            break
-        
-        if ch == '/':
-            row += 1
-            col = 0
-        elif ch.isdigit():
-            col += int(ch)
-        else:
-            if is_flipped:
-                result[piece_to_index[ch], 7 - col, row] = 1
-            else:
-                result[piece_to_index[ch], col, row] = 1
-            col += 1
-            
-    return result, is_flipped
+    def from_fen(self, fen):
+        result = np.zeros((12, 8, 8), dtype=np.int8)
 
-def simple_compact_representation(fen):
+        is_flipped = False
+        if fen.split(' ')[1] == 'b':
+            fen = fen.swapcase()
+            is_flipped = True
+    
+        row, col = 0, 0
+        for ch in fen:
+            if ch == ' ':
+                break
+        
+            if ch == '/':
+                row += 1
+                col = 0
+            elif ch.isdigit():
+                col += int(ch)
+            else:
+                if is_flipped:
+                    result[piece_to_index[ch], 7 - col, row] = 1
+                else:
+                    result[piece_to_index[ch], col, row] = 1
+                col += 1
+
+        return result, is_flipped
+    
+class CompactRepresentation:
     """
     * requires (6, 8, 8) input size
 
     7 panes, one per type of piece, +1 -> white, -1 -> black
     """
-    result = np.zeros((6, 8, 8), dtype=np.int8)
-
-    # Check if we need to flip the board
-    is_flipped = False
-    if fen.split(' ')[1] == 'b':
-        fen = fen.swapcase()
-        is_flipped = True
+    def get_input_size(self):
+        return (6, 8, 8)
     
-    row, col = 0, 0
-    for ch in fen:
-        if ch == ' ':
-            break
-        
-        if ch == '/':
-            row += 1
-            col = 0
-        elif ch.isdigit():
-            col += int(ch)
-        else:
-            if is_flipped:
-                if ch.isupper():
-                    result[piece_to_index[ch % 6], 7-col, row] = 1
-                else:
-                    result[piece_to_index[ch % 6], 7-col, row] = -1
-            else:
-                if ch.isupper():
-                    result[piece_to_index[ch % 6], col, row] = -1
-                else:
-                    result[piece_to_index[ch % 6], col, row] = 1
-            col += 1
-            
-    return result, is_flipped
+    def from_fen(self, fen):
+        result = np.zeros((6, 8, 8), dtype=np.int8)
 
-def create_model():
-    input_ = tf.keras.Input(shape=(12,8,8,), dtype='int8')
+        # Check if we need to flip the board
+        is_flipped = False
+        if fen.split(' ')[1] == 'b':
+            fen = fen.swapcase()
+            is_flipped = True
+
+        row, col = 0, 0
+        for ch in fen:
+            if ch == ' ':
+                break
+
+            if ch == '/':
+                row += 1
+                col = 0
+            elif ch.isdigit():
+                col += int(ch)
+            else:
+                if is_flipped:
+                    if ch.isupper():
+                        result[piece_to_index[ch % 6], 7-col, row] = 1
+                    else:
+                        result[piece_to_index[ch % 6], 7-col, row] = -1
+                else:
+                    if ch.isupper():
+                        result[piece_to_index[ch % 6], col, row] = -1
+                    else:
+                        result[piece_to_index[ch % 6], col, row] = 1
+                col += 1
+
+        return result, is_flipped
+
+class HalfKP:
+    """
+    * requires (10, 8, 8, 8, 8) input size
+
+    10 panes, one per type of piece
+
+    TODO: requires sparse inputs, a list of classes
+    """
+    def get_input_size(self):
+        return (6, 8, 8)
+
+    def from_fen(fen):
+        pass
+
+def create_model(representation):
+    # TODO: get input size / layer from representation...
+    input_ = tf.keras.Input(shape=representation.get_input_size(), dtype='int8')
     flat = tf.keras.layers.Flatten()(input_)
-    hidden1 = tf.keras.layers.Dense(768, activation='elu',
+    hidden1 = tf.keras.layers.Dense(1024, activation='elu',
                                     #kernel_regularizer=tf.keras.regularizers.L2(1e-9)
                                     )(flat)
     dropout1 = tf.keras.layers.Dropout(0.1)(hidden1)
@@ -109,7 +132,7 @@ def create_model():
     model.compile(optimizer="Adam", loss="mse")
     return model
 
-def load_data(filename, representation_fn):
+def load_data(filename, representation):
     df = pd.read_csv(filename)
 
     sample = []
@@ -120,8 +143,8 @@ def load_data(filename, representation_fn):
         if i >= 2000000:
             break
 
-        representation, is_flipped = representation_fn(fen)
-        sample.append(representation)
+        input_layer, is_flipped = representation.from_fen(fen)
+        sample.append(input_layer)
         # score = score * 10
         a, b = wins + score, total - wins + (10 - score)
 
@@ -138,8 +161,9 @@ def load_data(filename, representation_fn):
 
 
 if __name__ == '__main__':
-    model = create_model()
-    sample, ys = load_data('../output/all_scored.csv', simple_representation)
+    representation = BasicRepresentation()
+    model = create_model(representation)
+    sample, ys = load_data('./output/all_scored.csv', representation)
 
     print(sample.shape, ys.shape)
 
@@ -157,10 +181,11 @@ if __name__ == '__main__':
     print(np.mean(ys,axis=0), np.std(ys, axis=0))
     print(sample.shape)
 
-    save_callback = tf.keras.callbacks.ModelCheckpoint(filepath="models/chess-score-{epoch:04d}-{val_loss:.3f}.h5",
-                                                       save_weights_only=False,
-                                                       save_best_only=True,
-                                                       verbose=1)
+    save_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath="../models/chess-score-{epoch:04d}-{val_loss:.4f}.h5",
+        save_weights_only=False,
+        save_best_only=True,
+        verbose=1)
 
     # Train the model with the new callback
     history = model.fit(x_train, y_train,
